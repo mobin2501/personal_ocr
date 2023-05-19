9d839f5f25769ae374967d889044db6b8309e119
mobin <mobin2501@gmail.com>
2023-05-20 01:29:34 +0600

    moving the project to asus

--- ocr_project/app/forms.py
+++ ocr_project/app/forms.py
@@ -1,7 +1,11 @@
-from django.forms import forms,ModelForm
+from django import forms
 from .models import *
 
-class OcrUploadForm(ModelForm):
+class OcrUploadForm(forms.ModelForm):
     class Meta:
         model = Books        
         fields = ('file',)
+        field_class = 'flex bg-orange-400 text-red-100 py-3'
+        widgets = {
+            'file': forms.FileInput(attrs={'class': field_class, 'placeholder': 'upload an image'}),
+        }
